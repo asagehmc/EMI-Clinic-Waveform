6048deb217f8b203a0bd2ae4d4c5987560a65b09
Adam <asage@hmc.edu>
2025-02-04 09:11:26 -0800

    added visualization, updates to scanner

--- sizing.py
+++ sizing.py
@@ -1,11 +1,14 @@
 import json
 
 
+# 2000 patients roughly with ECG I lead
+
 if __name__ == "__main__":
-    with open("filtered_ABP_II.txt", "r") as f:
+    with open("filtered_ABP_I.txt", "r") as f:
         sum = 0
         a = json.load(f)
         for key, value in a.items():
             for segment in value:
                 sum += segment["size"]
-    print(sum)
+    print("num_entries", len(a))
+    print("data size", sum)
---
+++ visualization/visualization.py
@@ -0,0 +1,4 @@
+import wfdb
+
+if __name__ == "__main__":
+    wfdb.plot_all_records("data")
